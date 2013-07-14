6d7b626683c6817018a025380c14290c9ed2096f
William Povell <willipovell@gmail.com>
2013-07-13 22:50:16 -0400

    Added timestamp to logging.

--- log.py
+++ log.py
@@ -1,4 +1,6 @@
+import datetime
 def log(filename, message):
 	with open(filename, 'a') as f:
+		f.write(datetime.datetime.time(datetime.datetime.now()).strftime("%I:%M:%S %p "))
 		f.write(message)
 		f.write("\n")
